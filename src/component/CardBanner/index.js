9bb7f1f0f648617080233164890d119faed602c7
thisisismail <simail.official@gmail.com>
2022-07-02 18:38:18 +0700

    second commit

--- src/component/CardBanner/index.js
+++ src/component/CardBanner/index.js
@@ -20,23 +20,17 @@ export default function CardBanner() {
         </Card>
       </div>
       <div
-        style={{ minWidth: 574, maxWidth: 574 }}
+        style={{ maxWidth: 574 }}
         className="w-full px-4 py-6 flex flex-col justify-between gap-3 "
       >
-        <Typography
-          variant="h1"
-          className="text-right font-mono text-green-700"
-        >
+        <h1 className="text-right font-mono font-bold text-green-700 text-3xl md:text-5xl">
           Temukan dan kembagnkan potensi dirimu dan raih masa depan cemerlang
-        </Typography>
-        <Typography
-          variant="h5"
-          className="text-right font-thin font-mono text-grey-600"
-        >
+        </h1>
+        <h5 className="text-right font-thin font-mono text-grey-600 text-sm md:text-xl">
           Galau cari kerja? Mau mengembangkan diri tanpa pusing memikirkan
           biaya? Bekali dirimu dengan{" "}
           <span className="font-bold">Portal Karir</span> dan #RaihKarirSekarang
-        </Typography>
+        </h5>
       </div>
     </div>
   );
