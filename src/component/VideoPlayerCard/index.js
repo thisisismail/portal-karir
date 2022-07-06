0b875fdc3f05c0a7e32282aeeac118ff6b92f982
thisisismail <simail.official@gmail.com>
2022-07-07 00:24:44 +0700

    popup-player-added-rev-2

--- src/component/VideoPlayerCard/index.js
+++ src/component/VideoPlayerCard/index.js
@@ -7,11 +7,13 @@ export default function VideoPlayer(props) {
   const dispatch = useDispatch();
   return (
     <div
-      style={{ maxWidth: 800 }}
       id="played-video-id"
-      className="border-0 bg-grey-700 bg-opacity-80 backdrop-blur-sm h-full w-full px-0 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+      className="border-2 bg-grey-700 bg-opacity-80 backdrop-blur-sm h-full w-screen px-0 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
     >
-      <Card className="p-2 rounded-xl fixed top-1/2 -translate-y-1/2 mx-2">
+      <Card
+        style={{ maxWidth: 800 }}
+        className="p-2 rounded-xl fixed top-1/2 -translate-y-1/2 mx-auto"
+      >
         <iframe
           title="notitle"
           className="w-full aspect-video absolute sticky z-50 top-0 mb-2 rounded-xl"
