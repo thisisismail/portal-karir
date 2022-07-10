144308bfbaeaff949a9a6b0cbde61de0d30f1093
thisisismail <simail.official@gmail.com>
2022-07-10 18:06:35 +0700

    2-oauth-try

--- src/component/LoginButton/index.js
+++ src/component/LoginButton/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
 import { useDispatch, useSelector } from "react-redux";
 import { userData } from "../../store/action/index.js";
+import { FaGoogle } from "react-icons/fa";
 
 export default function LoginButton() {
   const [user, setUser] = useState({});
@@ -36,13 +37,14 @@ export default function LoginButton() {
 
     google.accounts.id.renderButton(document.getElementById("signalDiv"), {
       type: "icon",
-      theme: "outline",
+      theme: "filled_blue",
       size: "large",
+      shape: "circle",
     });
   }, []);
 
   return (
-    <div className="w-full border-0 border-red-200">
+    <div className="w-full border-2 h-full border-red-200">
       <div id="signalDiv"></div>
     </div>
   );
